a221580be7e55c6e3b1a00cb3b408fa0ec010473
joeclark-phd <joe@joeclark.net>
2019-08-06 15:49:03 -0400

    add a couple of tests of authentication-authorization

--- src/test/java/net/joeclark/webapps/granite/home/HomeControllerTest.java
+++ src/test/java/net/joeclark/webapps/granite/home/HomeControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -25,11 +26,26 @@ class HomeControllerTest {
     @Test
     public void greetingShouldReturnMessageFromService() throws Exception {
         when(service.greeting()).thenReturn("Bonjour");
-        this.mockMvc.perform(get("/"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Bonjour World")))
-        ;
+        mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Bonjour World")));
+    }
+
+    @Test
+    public void homePageShouldBeBlockedToUnauthorizedGuests() throws Exception {
+        mockMvc.perform(get("/home"))
+            .andDo(print())
+            .andExpect(status().is3xxRedirection());
+    }
+
+    @WithMockUser(value="user")
+    @Test
+    public void homePageShouldBeAccessibleToAuthorizedUser() throws Exception {
+        mockMvc.perform(get("/home"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Your Home Page")));
     }
 
 }
