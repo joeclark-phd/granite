f8b79575687b4050579240c010443b23fb14f076
joeclark-phd <joe@joeclark.net>
2019-08-07 09:51:54 -0400

    add security tests and a favicon

--- src/main/java/net/joeclark/webapps/granite/WebSecurityConfig.java
+++ src/main/java/net/joeclark/webapps/granite/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package net.joeclark.webapps.granite;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers("/").permitAll() // Homepage (only) will be viewable by anyone not logged in.
+                .antMatchers("/favicon.ico").permitAll() // Spring Boot looks for a favicon in src/main/resources and points this URL at it. Delete this line to use the default (leaf) favicon.
                 .antMatchers("/webjars/**").permitAll() // This directory is where Maven downloads bootstrap, jquery, etc.
                 .antMatchers("/public/**").permitAll() // A directory for images + CSS. I hope that naming it 'public' cautions future developers against over-using it.
                 .anyRequest().authenticated() // All other URL endpoints require authentication or will redirect users to the login page.
---
+++ src/test/java/net/joeclark/webapps/granite/home/HomeControllerMVCTest.java
@@ -0,0 +1,97 @@
+package net.joeclark.webapps.granite.home;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerMVCTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GreetingService service;
+
+    @Test
+    public void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.greeting()).thenReturn("Bonjour");
+        mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Bonjour World")));
+    }
+
+
+    @Nested
+    @DisplayName("Tests of the security configuration...")
+    class TestsOfSecurity {
+
+        @Test
+        @DisplayName("The index page / should be publicly accessible.")
+        public void indexPageShouldBePubliclyAccessible() throws Exception {
+            mockMvc.perform(get("/"))
+                    .andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("Images in the /public folder should be publicly accessible.")
+        public void publicFolderShouldBePubliclyAccessible() throws Exception {
+            mockMvc.perform(get("/public/granite.jpg"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.IMAGE_JPEG));
+        }
+
+        @Test
+        @DisplayName("The /images folder should be blocked to those not logged in.")
+        public void imagesFolderShouldBeBlockedToUnauthorizedGuests() throws Exception {
+            // The /images folder is public in the default Spring Boot Security configuration, so this test makes
+            // sure we've successfully overridden that.  Use the /public folder for anything that should be public.
+            mockMvc.perform(get("/images/granite.jpg"))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Test
+        @DisplayName("The /css folder should be blocked to those not logged in.")
+        public void cssFolderShouldBeBlockedToUnauthorizedGuests() throws Exception {
+            // The /css folder is public in the default Spring Boot Security configuration, so this test makes
+            // sure we've successfully overridden that.  Use the /public folder for anything that should be public.
+            mockMvc.perform(get("/css/bootstrap.css"))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Test
+        @DisplayName("The /home page should be blocked to those not logged in.")
+        public void homePageShouldBeBlockedToUnauthorizedGuests() throws Exception {
+            mockMvc.perform(get("/home"))
+                    .andDo(print())
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @WithMockUser(value="user")
+        @Test
+        @DisplayName("The /home page should be accessible to logged-in users.")
+        public void homePageShouldBeAccessibleToAuthorizedUser() throws Exception {
+            mockMvc.perform(get("/home"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(containsString("Your Home Page")));
+        }
+
+    }
+
+
+}
