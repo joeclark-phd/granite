4efc65d201ddd9a15566eae02af0adc66b19120d
joeclark-phd <joe@joeclark.net>
2019-08-13 15:54:27 -0400

    add implicity-deny to security config

--- src/main/java/net/joeclark/webapps/granite/WebSecurityConfig.java
+++ src/main/java/net/joeclark/webapps/granite/WebSecurityConfig.java
@@ -17,9 +17,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
+        web.ignoring().mvcMatchers(
                 "/favicon.ico",  // Spring Boot looks for a favicon in src/main/resources and points this URL at it. Delete this line to use the default (leaf) favicon.
-                "/webjars/**", // This directory is where Maven downloads bootstrap, jquery, etc.
+                 "/webjars/**", // This directory is where Maven downloads bootstrap, jquery, etc.
                 "/public/**" // A directory for static images + CSS. I hope that naming it 'public' cautions future developers against over-using it.
         );
     }
@@ -27,15 +27,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/").permitAll() // This/these endpoint(s) will be viewable by anyone not logged in.
-                .anyRequest().authenticated() // All other URL endpoints require authentication or will redirect users to the login page.
-            .and()
             .formLogin()
                 .loginPage("/login").permitAll()
-            .and()
+                .and()
             .logout()
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/")
+                .and()
+            .authorizeRequests()
+                .mvcMatchers("/").permitAll() // This/these endpoint(s) will be viewable by anyone not logged in.
+                .mvcMatchers("/home").authenticated() // All other URL endpoints require authentication or will redirect users to the login page.
+                .anyRequest().denyAll(); // Deny-by-default of any unauthorized or malformed URL is a security best practice.
         logger.debug("Executed custom security configuration for Granite project.");
     }
 
