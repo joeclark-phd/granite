1d78cd229b3f2fd359f629baa433f9d172ff77c1
joeclark-phd <joe@joeclark.net>
2019-10-02 09:05:57 -0400

    separate out MVC and security tests on homecontroller

--- src/test/java/net/joeclark/webapps/granite/home/HomeControllerIntegrationTest.java
+++ src/test/java/net/joeclark/webapps/granite/home/HomeControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package net.joeclark.webapps.granite.home;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,6 +23,7 @@ public class HomeControllerIntegrationTest {
     private MockMvc mockMvc;
 
     @Test
+    @DisplayName("The HomeController and GreetingService work together to retrieve a greeting message.")
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andDo(print())
--- src/test/java/net/joeclark/webapps/granite/home/HomeControllerMVCTest.java
+++ src/test/java/net/joeclark/webapps/granite/home/HomeControllerMVCTest.java
@@ -1,18 +1,14 @@
 package net.joeclark.webapps.granite.home;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Nested;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.not;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -29,6 +25,7 @@ class HomeControllerMVCTest {
     private GreetingService service;
 
     @Test
+    @DisplayName("The homepage should use a GreetingService to retrieve a greeting message.")
     public void greetingShouldReturnMessageFromService() throws Exception {
         when(service.greeting()).thenReturn("Bonjour");
         mockMvc.perform(get("/"))
@@ -37,67 +34,5 @@ class HomeControllerMVCTest {
             .andExpect(content().string(containsString("Bonjour World")));
         verify(service,times(1)).greeting(); // verifies that service.greeting() was called once
     }
-
-
-    @Nested
-    @DisplayName("Tests of the security configuration...")
-    class TestsOfSecurity {
-
-        @Test
-        @DisplayName("The index page / should be publicly accessible.")
-        public void indexPageShouldBePubliclyAccessible() throws Exception {
-            mockMvc.perform(get("/"))
-                    .andExpect(status().isOk());
-        }
-
-        @Test
-        @DisplayName("Images in the /public folder should be publicly accessible.")
-        public void publicFolderShouldBePubliclyAccessible() throws Exception {
-            mockMvc.perform(get("/public/granite3.jpg"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.IMAGE_JPEG));
-        }
-
-        @Test
-        @DisplayName("The /images folder should be blocked to those not logged in.")
-        public void imagesFolderShouldBeBlockedToUnauthorizedGuests() throws Exception {
-            // The /images folder is public in the default Spring Boot Security configuration, so this test makes
-            // sure we've successfully overridden that.  Use the /public folder for anything that should be public.
-            mockMvc.perform(get("/images/granite.jpg"))
-                    .andExpect(status().is3xxRedirection());
-        }
-
-        @Test
-        @DisplayName("The /css folder should be blocked to those not logged in.")
-        public void cssFolderShouldBeBlockedToUnauthorizedGuests() throws Exception {
-            // The /css folder is public in the default Spring Boot Security configuration, so this test makes
-            // sure we've successfully overridden that.  Use the /public folder for anything that should be public.
-            mockMvc.perform(get("/css/bootstrap.css"))
-                    .andExpect(status().is3xxRedirection());
-        }
-
-        @Test
-        @DisplayName("The index page / should show certain contanet ONLY to those not logged in.")
-        public void indexPageShowsCertainContentOnlyToThoseNotLoggedIn() throws Exception {
-            mockMvc.perform(get("/"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(content().string(containsString("Sign In")))
-                    .andExpect(content().string(not(containsString("You are authenticated."))));
-        }
-
-        @WithMockUser(value="user")
-        @Test
-        @DisplayName("The index page / should show certain content ONLY to logged-in users.")
-        public void indexPageShowsCertainContentOnlyToLoggedInUsers() throws Exception {
-            mockMvc.perform(get("/"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(content().string(containsString("You are authenticated.")))
-                    .andExpect(content().string(not(containsString("Sign In"))));
-        }
-
-    }
-
 
 }
